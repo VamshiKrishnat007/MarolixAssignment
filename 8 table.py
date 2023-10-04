16ea73c9b4acb7ce2ae6ebe4af192d4f98cc42e9
unknown <vamshikrishnat.marolix@gmail.com>
2023-10-04 21:35:40 +0530

    Dynamic multiplication table

--- 8 table.py
+++
@@ -1,2 +0,0 @@
-for loop in range(1,11):
-    print("8 * "+str(loop)+" "+"="+str(8*loop))
---
+++ dynamic mutiplication table.py
@@ -0,0 +1,4 @@
+Take_input =int(input())
+
+for loop in range(1,11):
+    print(str(Take_input)+" * "+str(loop)+" "+"="+str(Take_input*loop))
