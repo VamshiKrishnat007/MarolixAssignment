3ab44c019f853651eec62e67bc57517ae80c9a83
unknown <vamshikrishnat.marolix@gmail.com>
2023-10-05 14:10:46 +0530

    age verfication

--- assignments/verify_age.py
+++ assignments/verify_age.py
@@ -1,5 +1,7 @@
 input_Number=int(input("Enter age : \n"))
 if input_Number<18:
     print("You are a minor")
-else:
+elif input_Number<65:
     print("You are an adult")
+else:
+    print("You are a senior citizen")
