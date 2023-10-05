ece09960de3ae370c79941c27bb83f6c9e7d954a
unknown <vamshikrishnat.marolix@gmail.com>
2023-10-05 10:39:37 +0530

    condtional statements

---
+++ conditional.py
@@ -0,0 +1,9 @@
+int_Number=int(input("Enter number :\n\n"))
+if int_Number%3==0 and int_Number%5==0:
+    print("FOOFAA")
+elif int_Number%5==0:
+    print("FAA")
+elif int_Number%3==0:
+    print("FOO")
+else:
+    print("FAAFOO")
