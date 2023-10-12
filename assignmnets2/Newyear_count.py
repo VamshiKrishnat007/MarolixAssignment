374c37eece2969f63e3f8f6390a4e9690ab12f49
unknown <vamshikrishnat.marolix@gmail.com>
2023-10-12 12:14:31 +0530

    assignments

--- assignmnets2/Newyear_count.py
+++ assignmnets2/Newyear_count.py
@@ -2,12 +2,15 @@
 # print the time left the next new year
 from datetime import datetime, timedelta
 
+from datetime import datetime, timedelta,time
 input1=input()
-datetime_ob=datetime.strptime(input1,"%b %d %Y %I:%M %p")
-date_2=datetime(datetime_ob.year,month=12,day=31,hour=12)
+date_obj=datetime.strptime(input1,"%b %d %Y %I:%M %p")
+date_obj2=datetime(date_obj.year+1,month=1, day=1)
 
-dur=(date_2-datetime_ob).days
-print("{} Hours {} Minutes".format(dur*24,""))
+days=(date_obj2-date_obj)
+hours=days.days*24+days.seconds//3600
+minutes=(days.seconds//60)%60
+print("{} hours {} minutes".format(hours,minutes))
 
 
 
